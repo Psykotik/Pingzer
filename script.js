205796210996ace2a6d8333ba33fa6cf9fcd06e0
Psykotik <lambert.jord@gmail.com>
2017-08-28 21:04:18 +0200

    [Add] Added coloration based on ping
    Added panel of color based on how high ping is

--- script.js
+++ script.js
@@ -10,7 +10,35 @@ $.ajax({
         ping = new Date - ping;
         ping_calculated = ping * percentage;
         ping_calculated = Math.round(ping_calculated);
-        $('.ping').text("Ping " + ping + "ms");
-        $('.ping_calculated').text("Ping w/ calculation " + ping_calculated + "ms");
+        $('#ping').text("Raw ping " + ping + "ms");
+        $('#ping_calculated').text(ping_calculated + "ms");
+        console.log("ping");
+
+
+
+
+        if(ping>250) {
+          document.getElementById("result").classList.add('sup250');
+        } else if(ping>190) {
+          document.getElementById("result").classList.add('sup190');
+        }  else if(ping>175) {
+          document.getElementById("result").classList.add('sup175');
+        }  else if(ping>150) {
+          document.getElementById("result").classList.add('sup150');
+        }  else if(ping>135) {
+          document.getElementById("result").classList.add('sup135');
+        } else if(ping>120) {
+          document.getElementById("result").classList.add('sup120');
+        }  else if(ping>110) {
+          document.getElementById("result").classList.add('sup110');
+        }  else if(ping>90) {
+          document.getElementById("result").classList.add('sup90');
+        }else if(ping>75) {
+          document.getElementById("result").classList.add('sup75');
+        } else if(ping>50) {
+          document.getElementById("result").classList.add('sup50');
+        }  else if(ping<50) {
+          document.getElementById("result").classList.add('inf50');
+        }
 }
 });
